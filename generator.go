cc9d81f37326d28c533fbdf612726aada02883b0
Ripounet <rip0320@yahoo.com>
2017-12-30 21:36:32 +0100

    Struct type suffix (to avoid conflict with interface type) parameterized.

--- generator.go
+++ generator.go
@@ -27,7 +27,13 @@ func GenerateInterfaceTerminalStruct(ifc reflect.Type) []byte {
 
 	var gencode bytes.Buffer
 
-	err := ifcTmpl.Execute(&gencode, ifc)
+	err := ifcTmpl.Execute(&gencode, struct {
+		Ifc        reflect.Type
+		TypeSuffix string
+	}{
+		Ifc:        ifc,
+		TypeSuffix: "_X",
+	})
 	if err != nil {
 		panic(err)
 	}
@@ -43,15 +49,15 @@ func GenerateInterfaceTerminalStruct(ifc reflect.Type) []byte {
 // A struct can't have a Method and a Field with the same name.
 // So we introduce an intermediate name "Methods".
 const ifcTmplString = `
-type {{.Name}}_X struct {
+type {{.Ifc.Name}}{{.TypeSuffix}} struct {
 	Methods struct {
-		{{- range methods .}}
+		{{- range methods .Ifc}}
 		{{.Name}} {{.Type}}
 		{{- end}}
 	}
 }
-{{range methods .}}
-func (terminal {{$.Name}}_X) {{.Name}}({{- range $i, $arg := ins .Type -}}
+{{range methods .Ifc}}
+func (terminal {{$.Ifc.Name}}{{$.TypeSuffix}}) {{.Name}}({{- range $i, $arg := ins .Type -}}
 	a{{$i}} {{$arg}},
 {{- end}}) ({{- range $i, $arg := outs .Type -}}
 	out{{$i}} {{$arg}},
@@ -59,11 +65,18 @@ func (terminal {{$.Name}}_X) {{.Name}}({{- range $i, $arg := ins .Type -}}
 	if terminal.Methods.{{.Name}} == nil {
 		return
 	}
-	return terminal.Methods.{{.Name}}({{- range $i, $arg := ins .Type -}}
+	{{if outs .Type}}return {{end}}terminal.Methods.{{.Name}}({{- range $i, $arg := ins .Type -}}
 		a{{$i}},  
 	{{- end}})
 }
 {{end}}
+
+// This proves (by compiling) that {{.Ifc.Name}}{{.TypeSuffix}} implements {{.Ifc}}
+func init() {
+	var x {{.Ifc}} = {{.Ifc.Name}}{{.TypeSuffix}}{}
+	_ = x
+}
+
 `
 
 func methodsOf(t reflect.Type) (methods []reflect.Method) {
